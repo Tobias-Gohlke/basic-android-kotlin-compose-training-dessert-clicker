5d425c1974c819514188938a93ce97e0bcb45120
Tobias-Gohlke <85279053+Tobias-Gohlke@users.noreply.github.com>
2023-10-10 14:30:41 +0200

    Test

--- app/src/main/java/com/example/dessertclicker/MainActivity.kt
+++ app/src/main/java/com/example/dessertclicker/MainActivity.kt
@@ -25,7 +25,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.dessertclicker.ui.theme.DessertClickerTheme
-import com.example.dessertclicker.ui.
+import 
+
+
 
 
 private const val TAG = "MainActivity"
